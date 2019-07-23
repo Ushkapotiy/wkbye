a64baebea7d6a4d989cc1e4d9e30a77bafb1c009
Lame <47472916+Ushkapotiy@users.noreply.github.com>
2019-07-23 22:53:13 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 clear
-readonly ru=$(dirname $(readlink -f "$0"))
-source $ru/disig
+readonly st=$(dirname $(readlink -f "$0"))
+source $st/desig
 apt install mdk4
 apt install aircrack-ng
-chmod ugo+x $ru/language/ru.sh
-chmod ugo+x $ru/language/en.sh
-chmod ugo+x $ru/wkbye.sh
-chmod ugo+x $ru/source/source
-chmod ugo+x $ru/help/helpru.sh
-chmod ugo+x $ru/help/helpen.sh
+chmod ugo+x $st/language/ru.sh
+chmod ugo+x $st/language/en.sh
+chmod ugo+x $st/wkbye.sh
+chmod ugo+x $st/source/source
+chmod ugo+x $st/help/helpru.sh
+chmod ugo+x $st/help/helpen.sh
 clear
